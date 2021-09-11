0f7d21397b913a11aef1c74b5d683397bac47a24
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2021-09-12 00:34:54 +0300

    :sparkles: Add todos slice and setup redux store

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,48 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    list: [],
+    visibilityFilter: 'active'
+};
+
+export const todosSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            if (action.payload) {
+                const previousId = state.list[state.list.length - 1]?.id ?? -1
+                state.list.push({
+                    id: previousId + 1,
+                    body: action.payload,
+                    completed: false,
+                });
+            }
+        },
+        removeTodo: (state, action) => {
+            const index = state.list.findIndex(todo => todo.id === action.payload.id)
+            if (index !== -1) state.list.splice(index, 1)
+        },
+        completeTodo: (state, action) => {
+            const index = state.list.findIndex(todo => todo.id === action.payload.id)
+            if (index !== -1) {
+                state.list[index].completed = true
+            }
+        },
+        editTodo: (state, action) => {
+
+        },
+        setVisibility: (state, action) => {
+            state.visibilityFilter = action.payload
+        },
+    }
+});
+
+export const {
+    addTodo,
+    removeTodo,
+    editTodo,
+    completeTodo,
+    setVisibility
+} = todosSlice.actions;
+export default todosSlice.reducer;
