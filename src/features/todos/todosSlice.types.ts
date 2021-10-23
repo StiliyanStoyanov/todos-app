749c8b90c6eef1866377a5416743fc8741c91e5a
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2021-10-23 20:31:09 +0300

    :recycle: Rename file and remove unused TodoCardProps type

---
+++ src/features/todos/todosSlice.types.ts
@@ -0,0 +1,38 @@
+export type TodosState = {
+    entities: {
+        [key: string]: {
+            id: string,
+            body: string,
+            completed: boolean,
+            important: boolean,
+            tags: string[],
+            created: number,
+            dueDate: Date | undefined
+        }
+    },
+    ids: string[],
+    filter: 'all' | 'important' | 'planned' | 'active' | 'completed',
+    selectedId: string | null,
+    placeholder: 'Add important todo' | 'Add planned todo' | 'What needs doing?',
+}
+
+export type Placeholders = {
+    basic: 'What needs doing?',
+    planned: 'Add planned todo',
+    important: 'Add important todo'
+}
+
+export type Filters = {
+    all: 'all',
+    important: 'important',
+    planned: 'planned',
+    completed: 'completed',
+    active: 'active'
+}
+
+export type AddTodoPayload = {
+    body: string,
+    completed?: boolean,
+    important?: boolean,
+    dueDate?: Date | undefined
+}
