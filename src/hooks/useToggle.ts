d4adc475d484c984f3bbf36512625658f27d3eab
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2021-10-27 21:44:16 +0300

    :recycle: Remove defaultValues :recycle: Update method names :recycle: Simplify types

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,21 +1,17 @@
 import {useMemo, useState} from "react";
-type AnyCouple = [any, any]
-type BooleanCouple = [boolean, boolean]
-type Methods = { toggle: () => void, open: () => void, close: () => void }
-type DefaultProps = [boolean, Methods];
-type ExtractProps<T extends AnyCouple> = [T[0] | T[1], Methods]
-type Return<T extends AnyCouple> = T extends BooleanCouple ? DefaultProps : ExtractProps<T>
-const defaultValues: BooleanCouple = [true, false]
-const useToggle = <T extends AnyCouple>(toggleValues: T | BooleanCouple = defaultValues): Return<T>  => {
+
+type ArrayCouple = [any, any]
+type Methods = { toggle: () => void, setIndex0: () => void, setIndex1: () => void }
+
+const useToggle = <T extends ArrayCouple>(toggleValues: T): [T[0] | T[1], Methods]  => {
     const [index, setIndex] = useState<0 | 1>(0);
-    const methods = useMemo(() => {
+    const methods: Methods = useMemo(() => {
         const toggle = () => setIndex(currentIndex => currentIndex ? 0 : 1)
-        const open = () => setIndex(0);
-        const close = () => setIndex(1);
-        return {toggle, open, close}
+        const setIndex0 = () => setIndex(0);
+        const setIndex1 = () => setIndex(1);
+        return {toggle, setIndex0, setIndex1}
     }, [setIndex]);
 
     return [toggleValues[index], methods];
 }
-
 export default useToggle
