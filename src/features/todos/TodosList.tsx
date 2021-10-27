5c9bc2bff07e5842501edd2658ab884dd7a6efa0
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2021-10-27 21:47:52 +0300

    :recycle: Change styled-components with @emotion/styled :recycle: Render nothing if list is empty :recycle: Update style

--- src/features/todos/TodosList.tsx
+++ src/features/todos/TodosList.tsx
@@ -1,21 +1,24 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import TodoCard from "./TodoCard";
 import {useSelector} from "react-redux";
 import {selectVisibleIds} from "./todosSlice";
 
 const TodosList = () => {
     const visibleIds = useSelector(selectVisibleIds)
+
+    if (visibleIds.length === 0) return null
     return (
         <Ul>
             {visibleIds.map(id => <TodoCard key={id} id={id}/>)}
         </Ul>
-    )
+    );
 };
 
 const Ul = styled.ul`
   text-decoration: none;
   list-style: none;
   padding: 0;
+  margin: 0;
   overflow: auto;
 `
 
